6d590668c629e8095aa32daf02191f78d0e13be1
techlover <e.rono@outlook.com>
2023-10-11 20:04:25 +0300

    updated components Charity & story

--- src/components/CharityForm.tsx
+++ src/components/CharityForm.tsx
@@ -1,78 +1,54 @@
-import React, { useState } from 'react';
-
-interface CharityFormProps {
-  onSubmit: (charityDetails: CharityDetails) => void;
+interface CharityDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  charityName: string;
+  charityDescription: string;
+  charityAmountGoal: number;
 }
 
-interface CharityDetails {
-    firstName: string;
-    lastName: string;
-    email: string;
-    charityName: string;
-    charityDescription: string;
-    charityamountGoal: number;
-  }
+const CharityForm: React.FC<CharityFormProps> = ({ onSubmit }) => {
+  const [charityDetails, setCharityDetails] = useState<CharityDetails>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    charityName: '',
+    charityDescription: '',
+    charityAmountGoal: 0,
+  });
 
-  const CharityForm: React.FC<CharityFormProps> = ({ onSubmit }) => {
-    const [charityDetails, setCharityDetails] = useState<CharityDetails>({
-      firstName: '',
-      lastName: '',
-      email: '',
-      charityName: '',
-      charityDescription: '',
-      amountGoal: 0,
-    });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setCharityDetails((prevDetails) => ({ ...prevDetails, [name]: value }));
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const { name, value } = e.target;
-      setCharityDetails((prevDetails) => ({ ...prevDetails, [name]: value }));
-    };
+  const handleCreateCharity = () => {
+    // I will add validation here ...
 
-    const handleCreateCharity = () => {
-      // I will add validation here ...
+    // Call the onSubmit callback with the charity details
+    onSubmit(charityDetails);
+  };
 
-      // Call the onSubmit callback with the charity details
-      onSubmit(charityDetails);
-    };
+  return (
+    <div>
+      <h2>Create Charity</h2>
+      <label>
+        First Name:
+        <input type="text" name="firstName" value={charityDetails.firstName} onChange={handleChange} />
+      </label>
+      <br />
+      {/* ... (other input fields) ... */}
+      <br />
+      <label>
+        Amount Goal:
+        <input type="number" name="charityAmountGoal" value={charityDetails.charityAmountGoal} onChange={handleChange} />
+      </label>
+      <br />
+      {/* Add Image Button (You might want to implement this) */}
+      <br />
+      <button onClick={handleCreateCharity}>Create Charity</button>
+    </div>
+  );
+};
 
-    return (
-        <div>
-          <h2>Create Charity</h2>
-          <label>
-            First Name:
-            <input type="text" name="firstName" value={charityDetails.firstName} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Last Name:
-            <input type="text" name="lastName" value={charityDetails.lastName} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Email:
-            <input type="text" name="email" value={charityDetails.email} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Charity Name:
-            <input type="text" name="charityName" value={charityDetails.charityName} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Charity Description:
-            <textarea name="charityDescription" value={charityDetails.charityDescription} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Amount Goal:
-            <input type="number" name="charityamountGoal" value={charityDetails.charityamountGoal} onChange={handleChange} />
-          </label>
-          <br />
-          {/* Add Image Button (You might want to implement this) */}
-          <br />
-          <button onClick={handleCreateCharity}>Create Charity</button>
-        </div>
-      );
-    };
-
-    export default CharityForm;
+export default CharityForm;
