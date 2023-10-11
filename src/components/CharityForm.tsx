1e32cf6afa9e22618a8658a75768e7351ed81437
techlover <e.rono@outlook.com>
2023-10-11 20:59:57 +0300

    Defining CharityFormData

--- src/components/CharityForm.tsx
+++ src/components/CharityForm.tsx
@@ -1,1 +1,18 @@
 import React, { useState, useEffect } from 'react';
+
+interface CharityFormProps {
+  onsubmit: (formData: CharityFormData) => void;
+
+}
+
+interface CharityFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  charityName: string;
+  charityDescription: string;
+  charityAmountGoal: number;
+  charityImage: File | null;
+}
+
+const
