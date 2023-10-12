c57aed7562e83abfd263726fc6c0e0cb1960cd2f
techlover <e.rono@outlook.com>
2023-10-12 19:54:52 +0300

    Finale

--- src/components/CharityForm.tsx
+++ src/components/CharityForm.tsx
@@ -120,74 +120,73 @@ const CharityForm: React.FC<CharityFormProps> = ({ onSubmit }) => {
         }
 
         // Future additional validations can be added here...
-
-        return true;
       };
+      return true;
+    };
 
-      const handleSubmit = () => {
-        if (validateForm()) {
-          onSubmit(formData);
-          setSuccessMessage(
-            'Registration is successful. Thank you for registering with us. Welcome to Donation APP'
-          );
-        }
-      };
-
-      useEffect(() => {
-        let timeout: NodeJS.Timeout;
-        if (successMessage) {
-          timeout = setTimeout(() => {
-            setSuccessMessage(null);
-          }, 60000); // Equivalent to 1 minute
-        }
-
-        return () => clearTimeout(timeout);
-      }, [successMessage]);
-
-      return (
-          <div>
-            <h2>Charity Form</h2>
-            <label>
-              First Name:
-              <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} />
-            </label>
-            <label>
-              Last Name:
-              <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
-            </label>
-            <label>
-              Email:
-              <input type="text" name="email" value={formData.email} onChange={handleInputChange} />
-            </label>
-            <label>
-              Charity Name:
-              <input type="text" name="charityName" value={formData.charityName} onChange={handleInputChange} />
-            </label>
-            <label>
-              Charity Description:
-              <textarea
-                name="charityDescription"
-                value={formData.charityDescription}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Charity Amount Goal:
-              <input
-                type="number"
-                name="charityAmountGoal"
-                value={formData.charityAmountGoal}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Charity Image:
-              <input type="file" accept=".jpg, .jpeg, .png, .svg" onChange={handleImageChange} />
-            </label>
-            <button onClick={handleSubmit}>Create Charity</button>
-            {successMessage && <p>{successMessage}</p>}
-          </div>
+    const handleSubmit = () => {
+      if (validateForm()) {
+        onSubmit(formData);
+        setSuccessMessage(
+          'Registration is successful. Thank you for registering with us. Welcome to Donation APP'
         );
-      };
+      }
+    };
 
+    useEffect(() => {
+      let timeout: NodeJS.Timeout;
+      if (successMessage) {
+        timeout = setTimeout(() => {
+          setSuccessMessage(null);
+        }, 60000); // Equivalent to 1 minute
+      }
+
+      return () => clearTimeout(timeout);
+    }, [successMessage]);
+
+    return (
+      <div>
+        <h2>Charity Form</h2>
+        <label>
+          First Name:
+          <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email" value={formData.email} onChange={handleInputChange} />
+        </label>
+        <label>
+          Charity Name:
+          <input type="text" name="charityName" value={formData.charityName} onChange={handleInputChange} />
+        </label>
+        <label>
+          Charity Description:
+          <textarea
+            name="charityDescription"
+            value={formData.charityDescription}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Charity Amount Goal:
+          <input
+            type="number"
+            name="charityAmountGoal"
+            value={formData.charityAmountGoal}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Charity Image:
+          <input type="file" accept=".jpg, .jpeg, .png, .svg" onChange={handleImageChange} />
+        </label>
+        <button onClick={() => {handleSubmit()}}>Create Charity</button>
+        {successMessage && <p>{successMessage}</p>}
+      </div>
+    )
+};
 export default CharityForm;
