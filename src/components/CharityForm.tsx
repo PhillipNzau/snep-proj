de6b7210903193da7953d9bf210944504d3a08c6
techlover <e.rono@outlook.com>
2023-10-12 11:19:24 +0300

    html part of it

--- src/components/CharityForm.tsx
+++ src/components/CharityForm.tsx
@@ -143,3 +143,51 @@ const CharityForm: React.FC<CharityFormProps> = ({ onSubmit }) => {
 
         return () => clearTimeout(timeout);
       }, [successMessage]);
+
+      return (
+          <div>
+            <h2>Charity Form</h2>
+            <label>
+              First Name:
+              <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} />
+            </label>
+            <label>
+              Last Name:
+              <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
+            </label>
+            <label>
+              Email:
+              <input type="text" name="email" value={formData.email} onChange={handleInputChange} />
+            </label>
+            <label>
+              Charity Name:
+              <input type="text" name="charityName" value={formData.charityName} onChange={handleInputChange} />
+            </label>
+            <label>
+              Charity Description:
+              <textarea
+                name="charityDescription"
+                value={formData.charityDescription}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Charity Amount Goal:
+              <input
+                type="number"
+                name="charityAmountGoal"
+                value={formData.charityAmountGoal}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Charity Image:
+              <input type="file" accept=".jpg, .jpeg, .png, .svg" onChange={handleImageChange} />
+            </label>
+            <button onClick={handleSubmit}>Create Charity</button>
+            {successMessage && <p>{successMessage}</p>}
+          </div>
+        );
+      };
+
+export default CharityForm;
